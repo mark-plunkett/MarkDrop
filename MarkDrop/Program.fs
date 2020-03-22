0973f04ad1fa13ecee5f505e517adc683be6f55d
mark.plunkett <markje.p@gmail.com>
2020-03-22 19:31:19 +0000

    Restructured solution to avoid nesting and added resource links.

--- MarkDrop/Program.fs
+++ MarkDrop/Program.fs
@@ -2,10 +2,13 @@
 
 open System
 
-open WavAudio
-
 [<EntryPoint>]
 let main argv =
+
+    // resources
+    // WAV format: https://web.archive.org/web/20141213140451/https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+    // FFT: https://en.wikipedia.org/wiki/Fast_Fourier_transform
+    // Cooley–Tukey FFT: https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm
 
     let fileName = @"C:\Dev\MarkDrop\Audio\test.wav"
     let file = System.IO.File.OpenRead(@"C:\Dev\MarkDrop\Audio\test.wav")
