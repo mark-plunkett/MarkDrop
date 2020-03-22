e0fdbcb7c5e6163c459eaceebbd1fae626223ae1
mark.plunkett <markje.p@gmail.com>
2020-03-22 19:21:59 +0000

    Added wav file info reader.

---
+++ MarkDrop/MarkDrop/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open WavAudio
+
+[<EntryPoint>]
+let main argv =
+
+    let fileName = @"C:\Dev\MarkDrop\Audio\test.wav"
+    let file = System.IO.File.OpenRead(@"C:\Dev\MarkDrop\Audio\test.wav")
+    
+    let wavFile = WavAudio.readWav file
+
+    wavFile |> WavAudio.printInfo fileName
+   
+    0
---
+++ MarkDrop/MarkDrop/WavAudio.fs
@@ -0,0 +1,73 @@
+﻿module WavAudio
+
+    open System
+
+    let headerSize = 42
+
+    type WavFile = {
+        ChunkID: string
+        ChunkSize: int
+        Format: string
+
+        SubChunk1ID: string
+        SubChunk1Size: int
+        AudioFormat: int16
+        NumChannels: int16
+        SampleRate: int
+        ByteRate: int
+        BlockAlign: int16
+        BitsPerSample: int16
+
+        SubChunk2ID: string
+        SubChunk2Size: int
+    }
+
+    let readWav fileStream =
+        use reader = new IO.BinaryReader(fileStream)
+        {
+            ChunkID = reader.ReadBytes(4) |> Text.Encoding.ASCII.GetString
+            ChunkSize = reader.ReadBytes(4) |> Convert.toInt32
+            Format = reader.ReadBytes(4) |> Text.Encoding.ASCII.GetString
+            SubChunk1ID = reader.ReadBytes(4) |> Text.Encoding.ASCII.GetString
+            SubChunk1Size = reader.ReadBytes(4) |> Convert.toInt32
+            AudioFormat = reader.ReadBytes(2) |> Convert.toInt16
+            NumChannels = reader.ReadBytes(2) |> Convert.toInt16
+            SampleRate = reader.ReadBytes(4) |> Convert.toInt32
+            ByteRate = reader.ReadBytes(4) |> Convert.toInt32
+            BlockAlign = reader.ReadBytes(2) |> Convert.toInt16
+            BitsPerSample = reader.ReadBytes(2) |> Convert.toInt16
+            SubChunk2ID = reader.ReadBytes(4) |> Text.Encoding.ASCII.GetString
+            SubChunk2Size = reader.ReadBytes(4) |> Convert.toInt32
+        }
+
+    let printInfo fileName wav = 
+        printf """
+File: %s
+ChunkID: %s
+ChunkSize: %i
+Format: %s
+SubChunk1ID: %s
+SubChunk1Size: %i
+AudioFormat: %i
+NumChannels: %i
+SampleRate: %i
+ByteRate: %i
+BlockAlign: %i
+BitsPerSample: %i
+SubChunk2ID: %s
+SubChunk2Size: %i
+        """
+            fileName
+            wav.ChunkID
+            wav.ChunkSize
+            wav.Format
+            wav.SubChunk1ID
+            wav.SubChunk1Size
+            wav.AudioFormat
+            wav.NumChannels
+            wav.SampleRate
+            wav.ByteRate
+            wav.BlockAlign
+            wav.BitsPerSample
+            wav.SubChunk2ID
+            wav.SubChunk2Size
